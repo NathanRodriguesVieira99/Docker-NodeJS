1c170435079a6a677176bd0c28a11d6588f246d7
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-06-07 15:59:03 -0300

    feat: create create-user-use-case

---
+++ src/useCases/create-user-use-case.ts
@@ -0,0 +1,37 @@
+import type { Prisma, User } from '@/generated/prisma';
+import { prisma } from '@/lib/prisma';
+import { UserAlreadyExistsError } from './errors/user-already-exists-error';
+
+interface CreateUserUseCaseProps {
+    create(user: User): Promise<User>;
+}
+
+export class CreateUserUseCase implements CreateUserUseCaseProps {
+    async findByEmail(email: string): Promise<User | null> {
+        const user = await prisma.user.findUnique({
+            where: {
+                email,
+            },
+        });
+
+        if (!user) {
+            return null;
+        }
+
+        return user;
+    }
+
+    async create(data: Prisma.UserCreateInput): Promise<User> {
+        const user = await prisma.user.create({
+            data,
+        });
+
+        const userWithSameEmail = await this.findByEmail(data.email);
+
+        if (userWithSameEmail) {
+            throw new UserAlreadyExistsError();
+        }
+
+        return user;
+    }
+}
