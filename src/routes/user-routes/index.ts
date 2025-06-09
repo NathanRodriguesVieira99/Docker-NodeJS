ca4526b8f772abc861182c568a193df793660ccc
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-06-09 17:17:13 -0300

    feat: add list and delete users routes

--- src/routes/user-routes/index.ts
+++ src/routes/user-routes/index.ts
@@ -1,6 +1,10 @@
 import { CreateUserController } from '@/controllers/create-user-controller';
+import { DeleteUserController } from '@/controllers/delete-user-controller';
+import { ListUsersController } from '@/controllers/list-users-controller';
 import type { FastifyInstance } from 'fastify';
 
 export function UserRoutes(app: FastifyInstance) {
   app.post('/users', CreateUserController);
+  app.get('/users', ListUsersController);
+  app.delete('/users/:id', DeleteUserController);
 }
