8d5e55b79d06a5494970667f7c3be60b0ab5fd83
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-06-14 18:50:14 -0300

    feat: implement findByEmail method in PrismaUserRepository

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -3,9 +3,23 @@ import { prisma } from '@/lib/prisma';
 
 export interface IUserRepository {
   create(user: User): Promise<User>;
+  findByEmail(email: string): Promise<User | null>;
 }
 
 export class PrismaUserRepository implements IUserRepository {
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  }
   async create(user: Prisma.UserCreateInput): Promise<User> {
     const prismaUser = await prisma.user.create({
       data: {
