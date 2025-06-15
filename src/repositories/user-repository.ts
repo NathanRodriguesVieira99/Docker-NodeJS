af43128d72a29729b033049f5c13fdfd83cfb889
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-06-15 15:52:15 -0300

    refactor: remove PrismaUserRepository implementation from user-repository.ts

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -1,32 +1,6 @@
-import type { Prisma, User } from '@/generated/prisma';
-import { prisma } from '@/lib/prisma';
+import type { User } from '@/entities/User';
 
 export interface IUserRepository {
   create(user: User): Promise<User>;
   findByEmail(email: string): Promise<User | null>;
-}
-
-export class PrismaUserRepository implements IUserRepository {
-  async findByEmail(email: string): Promise<User | null> {
-    const user = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    });
-
-    if (!user) {
-      return null;
-    }
-
-    return user;
-  }
-  async create(user: Prisma.UserCreateInput): Promise<User> {
-    const prismaUser = await prisma.user.create({
-      data: {
-        ...user,
-      },
-    });
-
-    return prismaUser;
-  }
 }
