d2450ec6abb87a629494184631d95a77f6bd626f
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-06-26 21:29:20 -0300

    feat: add LOG_lEVEL to environment schema

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -4,6 +4,7 @@ const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
   HOST: z.coerce.string().default('0.0.0.0'),
+  LOG_LEVEL: z.string().optional().default('info'),
 });
 
 const _env = envSchema.safeParse(process.env);
