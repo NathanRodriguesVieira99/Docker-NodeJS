77e63adb1cfe25e21f2be9acd3ffda21082e83c0
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-05 01:00:18 -0300

    feat(user-repository): implement findById method to user-repository interface

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -3,6 +3,7 @@ import type { User } from '@/entities/User';
 export interface IUserRepository {
   create(user: User): Promise<User>;
   findByEmail(email: string): Promise<User | null>;
+  findById(id: string): Promise<User | null>;
   delete(id: string): Promise<boolean>;
   listAll(): Promise<User[]>;
 }
