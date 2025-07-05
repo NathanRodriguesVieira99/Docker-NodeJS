45a5a56d32a9cf5d0d27b24cdf93455915d152ea
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-04 23:59:02 -0300

    feat(prisma): add logging to prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,7 @@
 import { PrismaClient } from '@/generated/prisma';
+import { env } from '@/config/env';
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+  log: env.NODE_ENV === 'dev' ? ['query', 'error', 'warn'] : ['error'],
+  datasourceUrl: env.DATABASE_URL,
+});
