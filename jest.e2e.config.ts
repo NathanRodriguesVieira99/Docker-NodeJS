da1898277a0b7905c8b2edf86f58bca5c85fbe0c
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-08 20:49:17 -0300

    refactor(jest e2e): update e2e test configuration for coverage collection

--- jest.e2e.config.ts
+++ jest.e2e.config.ts
@@ -4,6 +4,13 @@ import type { Config } from 'jest';
 const config: Config = {
   ...baseConfig,
   testMatch: ['**/*.e2e.test.(ts|js|tsx|jsx)'],
+  testPathIgnorePatterns: [],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*.{test,spec}.ts',
+    '!<rootDir>/src/generated/**',
+    '!<rootDir>/src/@types/**',
+  ],
 };
 
 export default config;
