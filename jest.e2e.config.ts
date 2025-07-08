c3c1a1a2a49e353a1d8211a143f89127143b77f8
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-08 20:56:38 -0300

    refactor(jest e2e): update testPathIgnorePatterns and maxWorkers configuration

--- jest.e2e.config.ts
+++ jest.e2e.config.ts
@@ -4,7 +4,8 @@ import type { Config } from 'jest';
 const config: Config = {
   ...baseConfig,
   testMatch: ['**/*.e2e.test.(ts|js|tsx|jsx)'],
-  testPathIgnorePatterns: [],
+  testPathIgnorePatterns: ['/node_modules/'],
+  maxWorkers: 1, 
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/**/*.{test,spec}.ts',
