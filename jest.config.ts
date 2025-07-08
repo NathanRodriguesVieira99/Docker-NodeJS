8f3a464fc28dd6e1b12fa4098d919eac1021ada6
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-07-08 20:49:59 -0300

    refactor(jest): enhance coverage collection and test matching configuration

--- jest.config.ts
+++ jest.config.ts
@@ -3,10 +3,21 @@ import type { Config } from 'jest';
 const config: Config = {
   roots: ['<rootDir>/src'],
   coverageDirectory: 'coverage',
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*.test.ts',
+    '!<rootDir>/src/**/*.e2e.test.ts',
+    '!<rootDir>/src/**/*.spec.ts',
+    '!<rootDir>/src/generated/**',
+  ],
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest',
   },
+  testMatch: [
+    '**/__tests__/**/*.(ts|js|tsx|jsx)',
+    '**/*.(test|spec).(ts|js|tsx|jsx)',
+  ],
+  testPathIgnorePatterns: ['.*\\.e2e\\.test\\.(ts|js|tsx|jsx)$'],
   testEnvironment: 'node',
   coverageProvider: 'v8',
   moduleNameMapper: {
